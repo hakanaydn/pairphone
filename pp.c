2bc39a225e3764be0a154049a1ec4500d1ebf7bc
hakanaydn <aydinhakan91@gmail.com>
2021-01-02 22:21:39 +0300

    melpe 1200 linux test

--- pp.c
+++ pp.c
@@ -1,23 +1,69 @@
-///////////////////////////////////////////////
-//
-// **************************
-//
-// Project/Software name: X-Phone
-// Author: "Van Gegel" <gegelcopy@ukr.net>
-//
-// THIS IS A FREE SOFTWARE  AND FOR TEST ONLY!!!
-// Please do not use it in the case of life and death
-// This software is released under GNU LGPL:
-//
-// * LGPL 3.0 <http://www.gnu.org/licenses/lgpl.html>
-//
-// You’re free to copy, distribute and make commercial use
-// of this software under the following conditions:
-//
-// * You have to cite the author (and copyright owner): Van Gegel
-// * You have to provide a link to the author’s Homepage: <http://torfone.org>
-//
-///////////////////////////////////////////////
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include "memory.h"
+#include "math.h"
+
+#include <sys/time.h> // for clock_gettime()
+#include <unistd.h> // for usleep()
+
+#include <melpe/melpe.h> // for usleep()
+
+#define SPEECH_FRAME 540 
+	
+int16_t inbuf[SPEECH_FRAME]; //recording from Mike
+unsigned char bits[11]; 
+int16_t* sp_ptr;
+int16_t outbuf[SPEECH_FRAME]; //recording from Mike
+
+struct timeval start, end;
+long secs_used,micros_used;
+
+int main(int argc, char *argv[] )
+{
+	FILE *f_speech;               /* File of speech data                   */
+	FILE *f_serial;               /* File of serial bits for transmission  */
+  	int16_t frame = 0;             /* frame counter */
+
+	printf("MELPE 1200 CODER TEST\n");
+
+	if ( (f_speech = fopen(argv[1], "rb")) == NULL) {
+		printf("%s - Error opening file  %s !!\n", argv[0], argv[1]);
+		exit(0);
+	}
+	printf(" Input speech file:  %s\n", argv[1]);
+
+	if ( (f_serial = fopen(argv[2], "wb")) == NULL) {
+		printf("%s - Error opening file  %s !!\n", argv[0], argv[2]);
+		exit(0);
+	}
+	printf(" Output bitstream file:  %s\n", argv[2]);
+
+	/*init melp-e*/
+	melpe_i();
+	
+	gettimeofday(&start, NULL);
+	while(fread(&inbuf, sizeof(int16_t), SPEECH_FRAME, f_speech) == SPEECH_FRAME)
+	{
+		melpe_a(bits,&inbuf );
+		melpe_s(&outbuf,bits); 
+
+		fwrite(&outbuf, sizeof(int16_t), SPEECH_FRAME , f_serial);
+
+		frame++;
+	}
+	gettimeofday(&end, NULL);
+
+	secs_used=(end.tv_sec - start.tv_sec); //avoid overflow by subtracting first
+	micros_used= ((secs_used*1000000) + end.tv_usec) - (start.tv_usec);
+
+	printf("Frame : %d micros_used: %d\n",frame,micros_used);
+
+	return 0;
+}
+
+
 
 //This is a main procedure of PairPhone testing software
 //one-thread implementation as a infinite loop contained procedures for:
@@ -25,7 +71,7 @@
 //-recording voice from mike, compressing, encrypting, modulating, sending baseband into line (TX)
 //-scan keyboard, processing. Suspending thread if no job (CTR)
 //---------------------------------------------------------------------------
-#ifdef _WIN32
+/*#ifdef _WIN32
 
  #include <stdlib.h>
  #include <stdio.h>
@@ -59,11 +105,14 @@
 #include "ctr.h" //scan keyboard, processing. Suspending thread if no job (CTR)
 #include "rx.h"  //receiving baseband, demodulating, decrypting, decompressing, playing over earphones
 #include "tx.h"	 //recording voice from mike, compressing, encrypting, modulating, sending baseband into line
+#include <melpe/melpe.h> // for usleep()
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[] )
 {
+
+
  int i=0;
-    
+
  printf("---------------------------------------------------------------\r\n");
  printf("   PairPhone v0.1a  Van Gegel, 2016  MailTo: torfone@ukr.net\r\n");
  printf("     P2P private talk over GSM-FR compressed voice channel\r\n");  
@@ -88,7 +137,7 @@ int main(int argc, char* argv[])
    return 0;
 }
 //---------------------------------------------------------------------------
-
+*/
 
 
 
